b5b6e86242fd39fc81bb4db46a5365839f3796f7
Alejandro Alzate Zapata - Ceiba Software <alejandro.alzate@ceiba.com.co>
2018-01-30 14:16:55 -0500

    correcciones menores

--- taller2/src/test/java/com/example/demo/Taller2ApplicationTests.java
+++ taller2/src/test/java/com/example/demo/Taller2ApplicationTests.java
@@ -3,9 +3,6 @@ package com.example.demo;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
 public class Taller2ApplicationTests {
