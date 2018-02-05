a700742692d8bc8d8c614334130c49bc0a833b38
Alejandro Alzate Zapata - Ceiba Software <alejandro.alzate@ceiba.com.co>
2018-02-05 15:57:24 -0500

    bajando deuda tecnica

--- taller2/src/main/java/com/parqueadero/controllers/ControladorEntradas.java
+++ taller2/src/main/java/com/parqueadero/controllers/ControladorEntradas.java
@@ -5,17 +5,20 @@ import com.parqueadero.models.Parqueadero;
 public class ControladorEntradas {
 	private String vehiculo;
     private String placa;
-    private Parqueadero parqueadero = new Parqueadero(20, 10, 1000, 500, 8000, 4000);
+    private Parqueadero parqueadero;
     private String carro = "Carro";
+    private String moto = "Moto";
+    
 
     public ControladorEntradas(String vehiculo, String placa) {
+    	parqueadero = new Parqueadero(20, 10, 1000, 500, 8000, 4000);
         this.vehiculo = vehiculo;
         this.placa = placa;
     }
 
     public String verificarPlaca(String dia){
         char letra = placa.charAt(0);
-        if (letra == 'A' && !(dia.equals("Lunes") || dia.equals("Domingo"))){
+        if (letra == 'A' && !("Lunes".equals(dia) || "Domingo".equals(dia))){
             return "Acceso denegado";
         }else{
             return "Bienvenido";
@@ -25,7 +28,7 @@ public class ControladorEntradas {
     public String verificarDisponibilidad(){
         if (vehiculo.equals(carro) && parqueadero.getCeldasCarro()==0){
             return "No hay cupos para carros";
-        }else if (vehiculo.equals("Moto") && parqueadero.getCeldasMoto() == 0){
+        }else if (vehiculo.equals(moto) && parqueadero.getCeldasMoto() == 0){
             return "No hay cupos para motos";
         }else {
             return "Puede ingresar el vehiculo";
