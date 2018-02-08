54c614f5b3f9adbbeac0e19e65e55331cddac4df
Alejandro Alzate Zapata - Ceiba Software <alejandro.alzate@ceiba.com.co>
2018-02-08 09:23:05 -0500

    correcciones menores

--- taller2/src/main/java/com/parqueadero/services/impl/SacarVehiculoService.java
+++ taller2/src/main/java/com/parqueadero/services/impl/SacarVehiculoService.java
@@ -1,14 +1,10 @@
 package com.parqueadero.services.impl;
 
 import com.parqueadero.models.CarroModel;
+import com.parqueadero.models.MotoModel;
 import com.parqueadero.services.VigilanteService;
 
 public class SacarVehiculoService implements VigilanteService{
-
-	@Override
-	public CarroModel addCarro(CarroModel carroModel) {
-		return null;
-	}
 
 	@Override
 	public boolean verificarPlaca(CarroModel carroModel, int dia) {
@@ -18,5 +14,17 @@ public class SacarVehiculoService implements VigilanteService{
 	@Override
 	public boolean verificarDisponibilidad() {
 		return false;
+	}
+
+	@Override
+	public void addCarro(CarroModel carroModel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addMoto(MotoModel motoModel) {
+		// TODO Auto-generated method stub
+		
 	}
 }
