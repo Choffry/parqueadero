5349b49d02f1dda44ef5dfb1d80c78cbb6dda68b
Alejandro Alzate Zapata - Ceiba Software <alejandro.alzate@ceiba.com.co>
2018-02-20 08:50:35 -0500

    Correcciones menores

--- taller2/src/test/java/com/example/demo/servicios/MeterVehiculoTest.java
+++ taller2/src/test/java/com/example/demo/servicios/MeterVehiculoTest.java
@@ -8,8 +8,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
