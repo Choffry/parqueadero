47efd7f6fa1bdbcc70796b762bdf3f422a4a182f
Alejandro Alzate Zapata - Ceiba Software <alejandro.alzate@ceiba.com.co>
2018-02-21 06:53:04 -0500

    conectando con el frontend

--- taller2/src/main/java/com/parqueadero/controllers/ParqueaderoController.java
+++ taller2/src/main/java/com/parqueadero/controllers/ParqueaderoController.java
@@ -6,6 +6,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.parqueadero.models.VehiculoModel;
 import com.parqueadero.models.VehiculosAdentro;
 import com.parqueadero.services.VigilanteService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/parqueadero")
 public class ParqueaderoController {
--- taller2/src/test/java/com/example/demo/Taller2ApplicationTests.java
+++ taller2/src/test/java/com/example/demo/Taller2ApplicationTests.java
@@ -3,8 +3,14 @@ package com.example.demo;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.parqueadero.ParqueaderoApplication;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=ParqueaderoApplication.class)
 public class Taller2ApplicationTests {
 
 	@Test
